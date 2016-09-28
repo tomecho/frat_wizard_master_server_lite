921bc56f6e157d25e4a350f96be2e1f96edadc5e
Tom Peck <tapeck@umass.edu>
2016-09-28 14:41:06 -0400

    location controller, partially finished with tests

--- spec/controllers/location_controller_spec.rb
+++ spec/controllers/location_controller_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe LocationController, type: :controller do
+  it 'get #index' do
+    u1 = create :user
+    u1l1 = create :location, user: u1
+    u1l2 = create :location, user: u1
+
+    u2 = create :user
+    u2l1 = create :location, user: u2
+    u2l2 = create :location, user: u2
+  
+    get :index
+    expect(response.body).to eq([u1l2,u2l2].to_json)
+  end
+
+  it 'get #show' do
+    l = create :location
+    get :show, id: l.to_param 
+    expect(response.body).to eq(l.to_json)
+  end
 
 end
