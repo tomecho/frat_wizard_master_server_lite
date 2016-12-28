091bd46744db553508bc81bda97d350dae5d9b15
Tom Peck <tapeck@umass.edu>
2016-12-27 22:10:10 -0500

    verify token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,10 +14,28 @@ class ApplicationController < ActionController::Base
         # test will force set @current_user
         render(:head, status: :unauthorized) && return unless @current_user
       else
-        unless use_facebook_token(request.headers['Authorization'])
-          render(:head, status: :unauthorized) 
+        success = authenticate_with_http_token do |token, _options|
+          use_facebook_token(token)
+        end
+
+        unless success && @current_user
+          render(:head, status: :unauthorized) && return
         end
       end
+    end
+  end
+
+  def verify_facebook_token
+    success = authenticate_with_http_token do |token, _options|
+      use_facebook_token(token)
+    end
+
+    if success && @current_user # verified and found user
+      render :head, status: 202 # accepted
+    elsif success && !@current_user # verified but no user found
+      render :head, status: 204
+    else # not verified
+      render :head, status: 401
     end
   end
 
@@ -31,9 +49,8 @@ class ApplicationController < ActionController::Base
     if fb && fb.code == 200
       email = JSON.parse(fb.body)['email'] # if this exists then facebook has verified the token and found user for it
       user = User.find_by email: email
-      session[:user] = user
       @current_user = user
-      return true if session[:user]
+      return true if email
     end
     false
   end
