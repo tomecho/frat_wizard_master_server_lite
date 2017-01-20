b97d9151c859fcc214e4f74114a354938da7b18c
Tom Peck <tapeck@umass.edu>
2017-01-19 21:43:35 -0500

    making a couple corrections here

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,28 +6,28 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
-  before_action :auth_user
+  before_action :auth_user, except: %i(verify_facebook_token)
 
   def auth_user
-    unless session[:user]
-      if Rails.env.test?
-        # test will force set @current_user
-        render(:head, status: :unauthorized) && return unless @current_user
-      else
-        success = authenticate_with_http_token do |token, _options|
-          use_facebook_token(token)
-        end
+    if Rails.env.test?
+      # test will force set @current_user
+      render(:head, status: :unauthorized) && return unless @current_user
+    else
+      success = false
+      authenticate_with_http_token do |token, _options|
+        success = use_facebook_token(token)
+      end
 
-        unless success && @current_user
-          render(:head, status: :unauthorized) && return
-        end
+      unless success && @current_user
+        render(:head, status: :unauthorized) && return
       end
     end
   end
 
   def verify_facebook_token
-    success = authenticate_with_http_token do |token, _options|
-      use_facebook_token(token)
+    success = false
+    authenticate_with_http_token do |token, _options|
+      success = use_facebook_token(token)
     end
 
     if success && @current_user # verified and found user
@@ -46,7 +46,7 @@ class ApplicationController < ActionController::Base
     uri = URI.parse "https://graph.facebook.com/me?fields=email&access_token=#{token}"
     fb = Net::HTTP.get_response(uri)
 
-    if fb && fb.code == 200
+    if fb && fb.code == '200'
       email = JSON.parse(fb.body)['email'] # if this exists then facebook has verified the token and found user for it
       user = User.find_by email: email
       @current_user = user
