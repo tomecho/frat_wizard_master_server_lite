378d08a94367b98a9f1de45f6dbc1fd616ebadde
Tom Peck <tapeck@umass.edu>
2017-05-30 22:52:21 -0400

    create migration

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,12 @@
 class Group < ActiveRecord::Base
   belongs_to :org
-  has_many :group_users
+
+  has_many :group_users, dependent: :destroy
   has_many :users, through: :group_users
+
+  has_many :groups_permissions, dependent: :destroy
+  has_many :permissions, through: :groups_permissions
+
   validates :name, :description, :org, presence: true
   validates :name, :description, uniqueness: { scope: :org_id }
 end
---
+++ db/migrate/20170531015523_add_permissions.rb
@@ -0,0 +1,7 @@
+class AddPermissions < ActiveRecord::Migration[5.0]
+  def change
+    create_table :permissions do |t|
+      t.timestamps
+    end
+  end
+end
