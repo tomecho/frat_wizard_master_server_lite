7ad4dc7d957e97e7c1c019ccbf632391cc3bb880
Tom Peck <tapeck@umass.edu>
2017-06-01 22:23:04 -0400

    no real reason to look test permission any more directly, should test its use under user permissions and group permissions

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -1,4 +1,5 @@
 class Permission < ActiveRecord::Base
+  # groups wont be very useful to look at, many groups over many orgs will have same permission
   has_many :group_permissions, dependent: :destroy
   has_many :groups, through: :group_permissions
 
