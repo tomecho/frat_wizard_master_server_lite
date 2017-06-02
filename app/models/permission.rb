8340aff80db28bc8faf2792d7c40a325b2545534
Tom Peck <tapeck@umass.edu>
2017-06-01 22:09:32 -0400

    all tests passing on basic permissions tests

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -2,5 +2,5 @@ class Permission < ActiveRecord::Base
   has_many :group_permissions, dependent: :destroy
   has_many :groups, through: :group_permissions
 
-  validates :action, :controller, unique: true
+  validates :action, :controller, uniqueness: true
 end
