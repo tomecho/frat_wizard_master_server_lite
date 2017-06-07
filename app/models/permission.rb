6ea626bd197c5b700efe752afb2122a54cc84af4
Tom Peck <tapeck@umass.edu>
2017-06-06 20:33:52 -0400

    copy paste perm stuff

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -4,4 +4,60 @@ class Permission < ActiveRecord::Base
   has_many :groups, through: :group_permissions
 
   validates :action, :controller, uniqueness: true
+
+  def self.update_permissions_table
+    ActiveRecord::Base.transaction do
+      delete_outdated_permissions
+      create_new_permissions
+    end
+  end
+
+  def name
+    name = "#{controller.humanize.titleize} - #{action.humanize.titleize}"
+    name += " - #{id_field}" if id_field
+    name
+  end
+
+  def model
+    return controller.classify.constantize
+  rescue NameError
+    return nil
+  end
+
+  def self.delete_outdated_permissions
+    # Delete all permissions that do not map to a valid controller action
+    Rails.application.eager_load!
+    Permission.all.find_each do |permission|
+      # first check if it is one of our special permissions
+      next if SPECIAL_PERMS.include?(permission.attributes.slice('action', 'controller'))
+
+      controller = ApplicationController.descendants.find { |c| c.name == "#{permission.controller}_controller".camelcase }
+      # Destroy the permission if the controller doesn't exist
+      if controller.nil?
+        permission.destroy!
+        next
+      end
+
+      # Destroy the permission if the action doesn't exist
+      action = ApplicationController.get_actions(controller).find { |a| a == permission.action.to_s }
+      if action.nil?
+        permission.destroy!
+        next
+      end
+    end
+  end
+
+  def self.create_new_permissions
+    # Create a new full access permission for all controller actions that do not have one
+    ApplicationController.descendants.each do |controller| # get all children and grand children
+      ApplicationController.get_actions(controller).each do |action|
+        Permission.find_or_create_by(controller: controller.name.gsub!('Controller', '').underscore, action: action, id_field: nil)
+      end
+    end
+
+    # Handle our special perms
+    SPECIAL_PERMS.each do |p|
+      Permission.where(p).first_or_create
+    end
+  end
 end
--- spec/models/permission_spec.rb
+++ spec/models/permission_spec.rb
@@ -3,4 +3,64 @@ RSpec.describe :permission, type: :model do
   it 'has a valid factory' do
     expect(build(:permission)).to be_valid
   end
+  it 'is invalid without a controller' do
+    expect(build(:permission, controller: nil)).not_to be_valid
+  end
+  it 'is invalid without an action' do
+    expect(build(:permission, action: nil)).not_to be_valid
+  end
+
+  it 'has special perms' do
+    expect(Permission.find_by controller: '*', action: '*').to be_a(Permission)
+  end
+
+  describe '#name' do
+    it 'returns a human readable string of the permission' do
+      p = create(:permission)
+      expect(p.name).to be_a(String)
+    end
+  end
+
+  describe '#model' do
+    it 'returns a reference to the model class that this permission references' do
+      p = create(:permission, controller: 'users')
+      expect(p.model).to eq(User)
+    end
+
+    it 'returns the nil if the permission does not link to a model' do
+      p = create(:permission)
+      expect(p.model).to be_nil
+    end
+  end
+
+  describe '.update_permissions_table' do
+    it 'adds new controller actions to the permissions table' do
+      Permission.update_permissions_table
+      expect(Permission.find_by(controller: 'test')).to be_nil
+
+      class TestController < ApplicationController
+        def index; end
+      end
+
+      Permission.update_permissions_table
+      expect(Permission.find_by(controller: 'test', action: 'index')).to be_a(Permission)
+    end
+
+    it 'deletes permissions with non-existant controllers' do
+      p = create(:permission)
+      expect(Permission.find_by(controller: p.controller, action: p.action)).to be_a(Permission)
+
+      Permission.update_permissions_table
+      expect(Permission.find_by(controller: p.controller, action: p.action)).to be_nil
+    end
+
+    it 'deletes permissions with non-existant actions' do
+      Permission.update_permissions_table
+      p = create(:permission, controller: Permission.all.first.controller, action: 'dummy_action')
+      expect(Permission.find_by(controller: p.controller, action: p.action)).to be_a(Permission)
+
+      Permission.update_permissions_table
+      expect(Permission.find_by(controller: p.controller, action: p.action)).to be_nil
+    end
+  end
 end
