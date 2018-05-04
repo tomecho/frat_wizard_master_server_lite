62768a540112f36f3540867ec610bc1143e076c1
Tom Peck <tom@tompeck.me>
2018-05-03 22:53:54 -0400

    Keep add a few new columns to user for full name (getting rid of first and last name) then add image col

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180504024325) do
+ActiveRecord::Schema.define(version: 20180504025219) do
 
   create_table "addresses", force: :cascade do |t|
     t.string   "street"
@@ -89,8 +89,6 @@ ActiveRecord::Schema.define(version: 20180504024325) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "email"
@@ -105,6 +103,8 @@ ActiveRecord::Schema.define(version: 20180504024325) do
     t.string   "last_sign_in_ip"
     t.string   "provider"
     t.string   "uid"
+    t.string   "name"
+    t.text     "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
