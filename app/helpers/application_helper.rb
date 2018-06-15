bd0daa7f66ec3801d0cf963489f753112e078284
Tom Peck <tom@tompeck.me>
2018-06-14 22:26:48 -0400

    Just copy and paste in a an existing helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -31,12 +31,38 @@ module ApplicationHelper
 	end
 
 	# only renders link if we can visit it
-	def safe_link_to(name = nil, options = nil, html_options = nil, &block)
-		method = 'get' || options[:method]
-		route = Rails.application.routes.recognize_path url, method: method
+	def safe_link_to(name = nil, url_options = nil, html_options = nil, &block)
+    # Convert given options to a usable url, this allows:
+    #   link_to(@object) do ... end
+    #   link_to "Link text", @model ...
+    #   link_to "Link text", model_method_path ...
+    #   link_to "Link text", model_method_path(@model.id, ...) ...
+    #   link_to "Link text", "http://www.google.com" ...
 
-		if @current_user.has_permission?(route[:controller], route[:action])
-			return ActionView::Helpers::UrlHelper.link_to(name, options, html_options)
-		end
+    # Use name to generate url if the first arg was an object
+    if name.is_a? ActiveRecord::Base
+      url = url_for(name)
+      options = url_options
+    else
+      url = url_for(url_options)
+      options = html_options
+    end
+
+    # Always create the link if it isn't a relative path and not permission controlled
+    return super(name, url_options, html_options, &block) unless url.start_with?('/')
+
+    # Assume get
+    method = 'get'
+    # Replace with specific method if provided, replacing put with patch to fix issue with some routes and rails 4
+    method = options[:method].to_s.gsub('put', 'patch') if options && options[:method]
+
+    # Try to recognize the route
+    route = Rails.application.routes.recognize_path url, method: method
+
+    # Return the link as normal if the current user has the permission
+    return super(name, url_options, html_options, &block) if @current_user.has_permission?(route[:controller], route[:action], route[:id])
+
+    # Raise an error if the link is hidden but the user is omni and should see all links
+    # raise "Link not rendered for omni user, please check this" if @current_user.groups.find_by(name: "omni").present?
 	end
 end
