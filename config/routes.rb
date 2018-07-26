9909d6549a54f28abdedc3c5977df69c458a9d2a
Tom Peck <tom@tompeck.me>
2018-07-25 22:23:04 -0400

    Move api routes to their own namespace in routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,24 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: "user/omniauth_callbacks" }
   get '/', to: 'application#home', as: 'home'
-  post '/verify_facebook', to: 'application#verify_facebook_token'
 
-  get '/location/within', to: 'location#within'
-  resources :location, except: %i(update)
+  namespace :api do
+    post '/verify_facebook', to: 'application#verify_facebook_token'
 
-  get '/user/:id/location', to: 'user#location'
-  post '/user/:id/use_org_claim_code', to: 'user#use_org_claim_code'
-  resources :user, except: %i(delete create)
+    get '/location/within', to: 'location#within'
+    resources :location, except: %i(update)
 
-  resources :orgs, except: %i(new edit)
-  resources :org_claim_codes, only: %i(show create destroy)
+    get '/user/:id/location', to: 'user#location'
+    post '/user/:id/use_org_claim_code', to: 'user#use_org_claim_code'
+    resources :user, except: %i(delete create)
 
-  post '/groups/:id/add_user', to: 'groups#add_user'
-  delete '/groups/:id/remove_user', to: 'groups#remove_user'
-  post '/groups/:id/add_permission', to: 'groups#add_permission'
-  delete '/groups/:id/remove_permission', to: 'groups#remove_permission'
-  resources :groups, except: %i(edit new)
+    resources :orgs, except: %i(new edit)
+    resources :org_claim_codes, only: %i(show create destroy)
+
+    post '/groups/:id/add_user', to: 'groups#add_user'
+    delete '/groups/:id/remove_user', to: 'groups#remove_user'
+    post '/groups/:id/add_permission', to: 'groups#add_permission'
+    delete '/groups/:id/remove_permission', to: 'groups#remove_permission'
+    resources :groups, except: %i(edit new)
+  end
 end
