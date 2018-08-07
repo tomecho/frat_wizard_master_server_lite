3f54f508a78367fc357a18fb97192cfe8f95254b
Tom Peck <tom@tompeck.me>
2018-08-06 21:39:09 -0400

    Update to use full controller path in check permission helper and update permission checks for tests

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,12 +20,13 @@ module ApplicationHelper
   end
 
   def has_permission?(request, user)
+    binding.pry
     auth_paths = ['/users/sign_in', '/users/auth/facebook/callback']
     excluded_paths = ['/', '/home', '/verify_facebook_token']
     if (auth_paths + excluded_paths).include?(request.path || '')
       return true
     else
-      return user && user.has_permission?(controller_name, action_name)
+      return user && user.has_permission?(controller_path, action_name)
     end
   end
 
