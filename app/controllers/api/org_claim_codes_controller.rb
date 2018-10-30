738c48c13dd56110019fdbd07804c79079cad2d3
Tom Peck <tom@tompeck.me>
2018-10-29 22:38:36 -0400

    Add show method to org claim code controller

--- app/controllers/api/org_claim_codes_controller.rb
+++ app/controllers/api/org_claim_codes_controller.rb
@@ -1,38 +1,40 @@
-module Api
-  class OrgClaimCodesController < ApplicationController
-    before_action :set_org_claim_code, only: %i(show destroy)
+class OrgClaimCodesController < ApplicationController
+  before_action :set_org_claim_code, only: %i(show destroy)
 
-    # TODO given a claim code show the org
-    def show
+  def show
+    render json: @claim
+  end
+
+  # create new claim code based on org_id
+  def create
+    # we already know this user has permission for org_claim_codes#create but we need to check they are part of this org
+    unless @current_user.orgs.map(&:id).include?(new_org_claim_code_params[:org_id].to_i) || @current_user.has_permission?('*', '*')
+      render json: { errors: ['user not authorized']}, status: :forbidden and return
     end
 
-    # create new claim code based on org_id
-    def create
-      # we already know this user has permission for org_claim_codes#create but we need to check they are part of this org
-      unless @current_user.orgs.map(&:id).include?(new_org_claim_code_params[:org_id].to_i) || @current_user.has_permission?('*', '*')
-        render json: { errors: ['user not authorized']}, status: :forbidden and return
-      end
-
-      claim = OrgClaimCode.new(new_org_claim_code_params)
-      if claim.save
-        render json: claim
-      else
-        render json: { errors: claim.errors.full_messages }, status: :unprocessable_entity
-      end
+    claim = OrgClaimCode.new(new_org_claim_code_params)
+    if claim.save
+      render json: claim
+    else
+      render json: { errors: claim.errors.full_messages }, status: :unprocessable_entity
     end
+  end
 
-    # TODO remove claim code by its id
-    def destroy
+  # TODO remove claim code by its id
+  def destroy
+  end
+
+  private
+
+  def find_org_claim_code
+    if id = params.require(:org_claim_codes).permit(:id)
+      @claim = OrgClaimCode.find(id)
+    elsif claim_code = params.require(:org_claim_codes).permit(:code)
+      @claim = OrgClaimCode.find_by_code(claim_code)
     end
+  end
 
-    private
-
-    def set_org_claim_code
-      params.require(:org_claim_codes).permit(:id)
-    end
-
-    def new_org_claim_code_params
-      params.require(:org_claim_codes).permit(:org_id)
-    end
+  def new_org_claim_code_params
+    params.require(:org_claim_codes).permit(:org_id)
   end
 end
